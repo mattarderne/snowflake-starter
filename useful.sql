e84bdb92bb060980195cad145fc7883dbd01722d
Adam Stone <31513760+foundinblank@users.noreply.github.com>
2019-06-20 13:24:18 +0100

    more discussion about granting schema to reporting

--- useful.sql
+++ useful.sql
@@ -9,6 +9,8 @@ GRANT SELECT ON FUTURE VIEWS IN SCHEMA "RAW"."<SCHEMA_NAME>" TO ROLE transformer
 -- NB use Post Hooks in your dbt_project.yml file to grant usage on dbt runs, eg add something like the below:
 /*
 on-run-end:                     # grants permissions on all newly created tables
+                                # if you have dev and prod schema, the reporting role will see both
+                                # to prevent that, you may want to just hardcode the prod schema (e.g., analytics.analytics)
      - 'grant usage on schema {{ target.schema }} to reporting'
      - 'grant select on all tables in schema {{ target.schema }} to reporting'
      - 'grant select on all views in schema {{ target.schema }} to reporting'
