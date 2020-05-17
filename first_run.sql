a602a3daffb1300a1a2d656455d58d683420c109
Matt Arderne <matthew.arderne@smecapital.com>
2020-05-17 09:08:13 +0000

    Update first_run.sql

--- first_run.sql
+++ first_run.sql
@@ -5,13 +5,13 @@ USE ROLE ACCOUNTADMIN;
 USE ROLE SYSADMIN;
 
 CREATE DATABASE "RAW";
-# This database contains your raw data. 
-# This is the landing pad for everything extracted and loaded, as well as containing external stages for data living in S3. 
-# Access to this database is strictly permissioned.
+-- This database contains your raw data. 
+-- This is the landing pad for everything extracted and loaded, as well as containing external stages for data living in S3. 
+-- Access to this database is strictly permissioned.
 
 CREATE DATABASE "ANALYTICS";
-# This database contains tables and views accessible to analysts and reporting. 
-# Everything in analytics is created and owned by dataform/dbt.
+-- This database contains tables and views accessible to analysts and reporting. 
+-- Everything in analytics is created and owned by dataform/dbt.
 
 
 -- (2) CREATE WAREHOUSES
@@ -23,8 +23,8 @@ AUTO_RESUME = TRUE
 MIN_CLUSTER_COUNT = 1 MAX_CLUSTER_COUNT = 1
 //SCALING_POLICY = 'STANDARD'
 ;
-# Tools like Fivetran and Stitch will use this warehouse to perform their regular loads of new data. 
-# We separate this workload from the other workloads because, at scale, loading can put significant strain on your warehouse and we don’t want to cause slowness for your BI users.
+-- Tools like Fivetran and Stitch will use this warehouse to perform their regular loads of new data. 
+-- We separate this workload from the other workloads because, at scale, loading can put significant strain on your warehouse and we don’t want to cause slowness for your BI users.
 
 
 CREATE WAREHOUSE "TRANSFORMING"
@@ -35,8 +35,8 @@ AUTO_RESUME = TRUE
 MIN_CLUSTER_COUNT = 1 MAX_CLUSTER_COUNT = 1
 //SCALING_POLICY = 'STANDARD'
 ;
-# This is the warehouse that dataform/dbt will use to perform all data transformations. 
-# It will only be in use (and charging you credits) when regular jobs are being run.
+-- This is the warehouse that dataform/dbt will use to perform all data transformations. 
+-- It will only be in use (and charging you credits) when regular jobs are being run.
 
 CREATE WAREHOUSE "REPORTING"
 WITH WAREHOUSE_SIZE = 'XSMALL'
@@ -46,22 +46,22 @@ AUTO_RESUME = TRUE
 MIN_CLUSTER_COUNT = 1 MAX_CLUSTER_COUNT = 1
 //SCALING_POLICY = 'STANDARD'
 ;
-# BI tools will connect to this warehouse to run analytical queries and report the results to end users. 
-# This warehouse will be spun up only when a user is actively running a query against it.
+-- BI tools will connect to this warehouse to run analytical queries and report the results to end users. 
+-- This warehouse will be spun up only when a user is actively running a query against it.
 
 -- (3) CREATE ROLES AND GRANT PRIVILEGES
 USE ROLE ACCOUNTADMIN;
 
 CREATE ROLE "LOADER";
-# Owns the tables in your raw database, and connects to the loadingwarehouse.
+-- Owns the tables in your raw database, and connects to the loadingwarehouse.
 
 CREATE ROLE "TRANSFORMER";
-# Has query permissions on tables in raw database and owns tables in the analytics database. 
-# This is for dataform/dbt developers and scheduled jobs.
+-- Has query permissions on tables in raw database and owns tables in the analytics database. 
+-- This is for dataform/dbt developers and scheduled jobs.
 
 CREATE ROLE "REPORTER";
-# Has permissions on the analytics database only. This role is for data consumers, such as analysts and BI tools. 
-# These users will not have permissions to read data from the raw database
+-- Has permissions on the analytics database only. This role is for data consumers, such as analysts and BI tools. 
+-- These users will not have permissions to read data from the raw database
 
 GRANT ALL PRIVILEGES ON WAREHOUSE "LOADING" TO ROLE "LOADER";
 GRANT ALL PRIVILEGES ON WAREHOUSE "TRANSFORMING" TO ROLE "TRANSFORMER";
